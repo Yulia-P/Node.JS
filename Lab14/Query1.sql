c345a5fa271b14faf64d5594f26e39d258668cb7
Yulia P <pochikovskaja@gmail.com>
2021-12-13 01:58:26 +0300

    Lab14

---
+++ Lab14/Query1.sql
@@ -0,0 +1,12 @@
+select * from FACULTY;
+
+select * from PULPIT;
+
+select * from SUBJECT;
+
+select * from AUDITORIUM_TYPE;
+
+select * from AUDITORIUM where AUDITORIUM = '102-2';
+
+select * from AUDITORIUM;
+
