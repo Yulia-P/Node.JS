8944f3e9938a34b8260bb077531a31d3536f9834
Yulia P <pochikovskaja@gmail.com>
2021-12-13 01:48:30 +0300

    Lab3

---
+++ Lab3/03-02.js
@@ -0,0 +1,32 @@
+var http = require('http');
+var url = require('url');
+var fs = require('fs');
+
+var fact = (n) => { return (n <= 1 ? 1 : fact(n - 1)*n); }
+
+http.createServer(function (request, response) {
+    let rc = JSON.stringify({ k: 0 });
+    let path = url.parse(request.url).pathname;
+
+    if (path === '/fact') {
+        let param = url.parse(request.url, true).query.k;
+        if (typeof param != 'undefined') {
+            let k = parseInt(param);
+            if (Number.isInteger(k)) {
+                response.writeHead(200, { 'Content-Type': 'application/json; charset=utf-8' });
+                response.end(JSON.stringify({ k: k, fact: fact(k) }));
+            }
+        }
+
+    }
+    else if (path === '/') {
+        let html = fs.readFileSync('index.html');
+        response.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
+        response.end(html);
+    }
+    else {
+        response.end(rc);
+    }
+}).listen(5000);
+
+console.log('Server running at http://localhost:5000/');
